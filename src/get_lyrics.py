112d592a5ecb9bb56b47d0edac3cb94e46adaf28
Bruno Carlos Vieira <45644699+brunocvs7@users.noreply.github.com>
2020-05-28 21:40:27 -0300

    consertei csv

--- src/get_lyrics.py
+++ src/get_lyrics.py
@@ -15,7 +15,6 @@ GROUP = '/legiao-urbana'
 LYRICS_NAME = 'song-name'
 TITLE_CLASS = 'cnt-head_title'
 LYRICS_CLASS = 'cnt-letra p402_premium'
-
 DATA_OUTPUT_PATH = 'src/data'
 DATA_OUTPUT_SUB_PATH = '/raw'
 DATA_OUTPUT_NAME = '/lyrics.csv'
@@ -26,14 +25,15 @@ ll = LyricsLink(website = URL_SITE+GROUP, lyrics_class = LYRICS_NAME)
 # retornando lista de links
 list_links_lyrics = ll.get_links()
 
-#consultando cada link e  recuperando as letras
+#loop que varre os links na lista list_links_lyrics e  então carrega a página e captura o campo de texto, que é a letra da música
+# No final teremos um dicionário com as chaves sendo os títulos das músicas e os valores sendro strings contendo a letra das músicas
 lyrics = {}
 for lyrics_url in list_links_lyrics:
     lt = LyricsText(lyrics_url = URL_SITE+lyrics_url, title_class = TITLE_CLASS, lyrics_class = LYRICS_CLASS)
     lyrics[lt.get_title()] = lt.get_lyrics_text()
 
 #salvando dados
-data_frame_lyrics = pd.DataFrame.from_dict(lyrics, orient='index',columns=['lyrics'])
-data_frame_lyrics.reset_index(inplace = True)
-data_frame_lyrics.rename(columns = {'index':'title'}, inplace = True)
-data_frame_lyrics.to_csv(DATA_OUTPUT_PATH+DATA_OUTPUT_SUB_PATH+DATA_OUTPUT_NAME)
+data_frame_lyrics = pd.DataFrame.from_dict(lyrics, orient='index',columns=['lyrics']) \
+                                .reset_index(inplace = True) \
+                                .rename(columns = {'index':'title'}, inplace = True) \
+                                .to_csv(DATA_OUTPUT_PATH+DATA_OUTPUT_SUB_PATH+DATA_OUTPUT_NAME, index = False)
